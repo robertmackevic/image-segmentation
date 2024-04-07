Fixed IoU calculation.

src/dataset.py
@@ -87,9 +87,9 @@ class COCODataset(Dataset):
         sample = self.samples[index]
         image = self.transform(Image.open(sample["image_filepath"]))
 
-        background_mask, masks = torch.ones(self.resolution, dtype=torch.uint8), []
+        background_mask, masks = torch.ones(self.resolution), []
         for mask in sample["masks"].values():
-            masks.append(Tensor(mask))
+            masks.append(torch.tensor(mask))
             background_mask[mask == 1] = 0
 
         masks.append(background_mask)

src/trainer.py
@@ -2,6 +2,7 @@ from os import makedirs, listdir
 from statistics import mean
 from typing import Tuple, List, Dict, Optional
 
+import torch
 from torch import no_grad, Tensor, logical_and, logical_or
 from torch.nn import CrossEntropyLoss, Module
 from torch.optim import Adam
@@ -82,12 +83,13 @@ class Trainer:
             target = batch[1].to(self.device)
 
             with no_grad():
-                output = self.model(source)
+                output: Tensor = self.model(source)
                 loss = self.loss_fn(output, target)
                 losses.append(loss.item())
 
+            predicted_labels = output.argmax(dim=1, keepdim=True)
             for idx, label in enumerate(self.config.classes):
-                iou[label].append(self.compute_iou(output[:, idx, ...], target[:, idx, ...]))
+                iou[label].append(self.compute_iou(torch.eq(predicted_labels, idx), target[:, idx, ...]))
 
         iou = {label: mean(values) for label, values in iou.items()}
         m_iou = mean(iou.values())
@@ -130,8 +132,8 @@ class Trainer:
         for label, value in iou.items():
             self.logger.info(f"\tIoU {label}: {value:.3f}")
 
-    def compute_iou(self, output: Tensor, target: Tensor) -> float:
-        output = (output > self.config.conf).float()
+    @staticmethod
+    def compute_iou(output: Tensor, target: Tensor) -> float:
         intersection = logical_and(output, target).sum()
         union = logical_or(output, target).sum()
         iou = intersection.float() / (union.float() + 1e-8)

src/visuals.py
@@ -2,9 +2,10 @@ from io import BytesIO
 
 import numpy as np
 import requests
+import torch
 from PIL import Image
 from matplotlib import pyplot as plt
-from torch import Tensor
+from torch import Tensor, no_grad
 from torch.nn import Module
 
 from src.utils import load_config, compose_transform, get_available_device
@@ -20,9 +21,11 @@ class Segmentor:
         self.colors = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 
     def segment(self, image: Tensor) -> None:
-        image = image.unsqueeze(0).to(self.device)
-        masks = self.model(image)
-        self.visualize_mask(image, masks[0])
+        with no_grad():
+            image = image.unsqueeze(0).to(self.device)
+            predicted_labels = self.model(image).argmax(dim=1)
+            masks = [torch.eq(predicted_labels, idx)[0] for idx in range(len(self.config.classes))]
+            self.visualize_mask(image[0], torch.stack(masks))
 
     def segment_url(self, url: str) -> None:
         image = Image.open(BytesIO(requests.get(url).content))
@@ -35,7 +38,7 @@ class Segmentor:
 
         for i in range(self.num_classes):
             mask = masks[:, :, i]
-            image[mask > 0] = (image[mask > 0] * 0.5 + self.colors[i % self.colors.shape[0]] * 0.5)
+            image[mask > 0] = (image[mask > 0] * 0.6 + self.colors[i % self.colors.shape[0]] * 0.4)
 
         plt.figure(figsize=(8, 8))
         plt.imshow(image)